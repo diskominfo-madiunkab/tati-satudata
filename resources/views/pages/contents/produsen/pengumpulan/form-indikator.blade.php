fd8e5d5ef17f421a19dc73639a3b1261b3a95545
programmertati <programmertati@gmail.com>
2025-06-17 07:55:43 +0700

    fix: remove readonly attribute from klasifikasi textarea fields in form-indikator and form-variabel to allow user input

--- resources/views/pages/contents/produsen/pengumpulan/form-indikator.blade.php
+++ resources/views/pages/contents/produsen/pengumpulan/form-indikator.blade.php
@@ -178,7 +178,7 @@
                                     <div class="col-sm-10">
                                         <textarea name="klasifikasi_penyajian"
                                             class="form-control {{ isset($klasifikasi_penyajian) ? ($klasifikasi_penyajian->accepted ? 'is-valid' : 'is-invalid') : '' }}"
-                                            style="height: 100px" spellcheck="false" placeholder="Klasifikasi Penyajian" disabled>{{ old('klasifikasi_penyajian', $getdata->indikator?->klasifikasi_penyajian ?? optional($data->standar)->klasifikasi) }}</textarea>
+                                            style="height: 100px" spellcheck="false" placeholder="Klasifikasi Penyajian">{{ old('klasifikasi_penyajian', $getdata->indikator?->klasifikasi_penyajian ?? optional($data->standar)->klasifikasi) }}</textarea>
 
                                         @if (isset($klasifikasi_penyajian) && !empty($klasifikasi_penyajian->comment))
                                             <p class="text-muted text-comment">Komentar:
@@ -496,7 +496,7 @@
                                     <label for="klasifikasi_penyajian" class="col-sm-2 col-form-label">Klasifikasi
                                         Penyajian</label>
                                     <div class="col-sm-10">
-                                        <textarea name="klasifikasi_penyajian" readonly
+                                        <textarea name="klasifikasi_penyajian"
                                             class="form-control {{ isset($klasifikasi_penyajian) ? ($klasifikasi_penyajian->accepted ? 'is-valid' : 'is-invalid') : '' }}"
                                             style="height: 100px" spellcheck="false" placeholder="Klasifikasi Penyajian">{{ old('klasifikasi_penyajian', optional($data->standar)->klasifikasi) }}</textarea>
                                         @if (isset($klasifikasi_penyajian) && !empty($klasifikasi_penyajian->comment))
